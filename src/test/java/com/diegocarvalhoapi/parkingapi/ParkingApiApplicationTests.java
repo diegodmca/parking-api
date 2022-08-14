61cfb2124f9af3646fede79ac5202860e7266b4f
Diego Carvalho <diegodmca@outlook.com>
2022-08-14 19:17:09 -0300

    Initial commit

---
+++ src/test/java/com/diegocarvalhoapi/parkingapi/ParkingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegocarvalhoapi.parkingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
